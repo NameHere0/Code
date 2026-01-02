fae99fc349683ae56e37170545dace36a4b48560
Gameing_8921 <flip.from.2013@gmail.com>
2026-01-02 21:35:22 +0000

    Lesson 1.3 added

---
+++ C++ (LEARNING)/Lesson 1.3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // for std::cout
+
+int main()
+{
+    std::cout << 5;       // print the literal number `5`
+    std::cout << -6.8;    // print the literal number `-6.~~7~~ (because i know there are going to be jokes about it)`
+    std::cout << 'H';     // print the literal character `H`
+    std::cout << "Hello"; // print the literal text `Hello`
+    std::cout << "What if i\n";
+    std::cout << "Does this count as text?";
+
+    return 0;
+}
